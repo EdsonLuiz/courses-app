d11a83bea97c4361f4b952c592ee492a0af2fc94
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-10 11:45:00 -0300

    Add ScrollView

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { Text } from 'react-native';
-import styled from 'styled-components'
-import Card from './components/Card';
+import React from "react";
+import { ScrollView, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import { Ionicons } from '@expo/vector-icons';
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require('./assets/avatar.jpg')} />
-        <Title>Welcome back, </Title>
-        <Name>Edson Luiz</Name>
-      </TitleBar>
-      <SubTitle>
-        Continue Learning
-      </SubTitle>
-      <Card />
+      <SafeAreaView>
+        <ScrollView>
+          <TitleBar>
+            <Avatar source={require("./assets/avatar.jpg")} />
+            <Title>Welcome back, </Title>
+            <Name>Edson Luiz</Name>
+            <Ionicons
+              name="ios-notifications"
+              size={32} color="#4775f2"
+              style={{
+                position: 'absolute',
+                right: 20,
+                top: 5
+              }}
+            />
+          </TitleBar>
+          <SubTitle>Continue Learning</SubTitle>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <Card
+              title="Styled components"
+              image={require("./assets/background2.jpg")}
+              caption="React antive"
+              logo={require("./assets/logo-react.png")}
+              subtitle="5 of 12 sections"
+            />
+            <Card
+              title="Styled components 2"
+              image={require("./assets/background2.jpg")}
+              caption="React antive"
+              logo={require("./assets/logo-react.png")}
+              subtitle="5 of 12 sections"
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
@@ -22,38 +49,37 @@ export default function App() {
 const Container = styled.View`
   flex: 1;
   background-color: #f0f3f5;
-`
+`;
 
 const Avatar = styled.Image`
-  width:  44px;
+  width: 44px;
   height: 44px;
-  border-radius: 22px ;
+  border-radius: 22px;
   margin-left: 20px;
   background-color: #000;
 
   position: absolute;
   top: 0;
   left: 0;
-
-`
+`;
 
 const TitleBar = styled.View`
   width: 100%;
   margin-top: 50px;
   padding-left: 80px;
-`
+`;
 
 const Title = styled.Text`
   font-size: 16px;
   color: #b8bece;
   font-weight: 500;
-`
+`;
 
 const Name = styled.Text`
   font-size: 20px;
   color: #3c4560;
-  font-weight:  900;
-`
+  font-weight: 900;
+`;
 
 const SubTitle = styled.Text`
   font-size: 16px;
@@ -62,4 +88,4 @@ const SubTitle = styled.Text`
   margin-left: 20px;
   margin-top: 50px;
   text-transform: uppercase;
-`
+`;
